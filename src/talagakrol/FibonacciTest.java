a3e34eb9abf9488440659b89e6125d6b01f7e931
talagabartosz <talaga.bartosz@gmail.com>
2015-05-01 22:42:28 +0200

    getElementLoop method implemented and tested

--- src/talagakrol/FibonacciTest.java
+++ src/talagakrol/FibonacciTest.java
@@ -12,5 +12,6 @@ public class FibonacciTest {
 		assertEquals(50, FibonacciLauncher.getElemntLoop(6).intValue());
 		assertEquals(60, FibonacciLauncher.getElemntLoop(7).intValue());
 		assertEquals(100, FibonacciLauncher.getElemntLoop(20).intValue());
+		assertEquals(144, FibonacciLauncher.getElemntLoop(12).intValue());
 	}
 }
