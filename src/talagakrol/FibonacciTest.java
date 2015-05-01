42ed9a34f041d57c0ea42ab10d2669632c9424a7
talagabartosz <talaga.bartosz@gmail.com>
2015-05-01 20:32:16 +0200

    Test class added

---
+++ src/talagakrol/FibonacciTest.java
@@ -0,0 +1,14 @@
+package talagakrol;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FibonacciTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
