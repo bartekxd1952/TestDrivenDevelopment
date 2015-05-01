380cf89da696fbef8fc973a03a6a7f8b0aa30304
talagabartosz <talaga.bartosz@gmail.com>
2015-05-01 22:40:35 +0200

    getElementLoop method implementation added

--- src/talagakrol/FibonacciLauncher.java
+++ src/talagakrol/FibonacciLauncher.java
@@ -11,8 +11,24 @@ public class FibonacciLauncher
 	
 	public static BigInteger getElemntLoop(int index)
 	{
-		
-		return BigInteger.ONE;
+		if (index < 1)
+			throw new IllegalArgumentException();
+		if (index == 1)
+			return new BigInteger("1");
+		if (index == 2)
+			return new BigInteger("1");
+
+		BigInteger a = new BigInteger("1");
+		BigInteger b = new BigInteger("1");
+		BigInteger c = new BigInteger("0");
+
+		for (int i = 3; i <= index; i++)
+		{
+			c = b.add(a);
+			a = b;
+			b = c;
+		}
+		return c;
 	}
 	
 	private static void printFoundedElement(int i)
--- src/talagakrol/FibonacciTest.java
+++ src/talagakrol/FibonacciTest.java
@@ -9,6 +9,8 @@ public class FibonacciTest {
 	@Test
 	public void getElementLoopTest()
 	{
-		assertEquals(50, FibonacciLauncher.getElemntLoop(6));
+		assertEquals(50, FibonacciLauncher.getElemntLoop(6).intValue());
+		assertEquals(60, FibonacciLauncher.getElemntLoop(7).intValue());
+		assertEquals(100, FibonacciLauncher.getElemntLoop(20).intValue());
 	}
 }
